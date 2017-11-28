263eafb2f14d5ced7b179a2df0ac269f4b63813e
COM1550\Administrator <stackeddeck_ben@hotmail.com>
2017-11-28 09:14:08 +0700

    Update 28-11-2017

---
+++ src/main/java/com/wacoal/dockeeper/MainController.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wacoal.dockeeper;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author Administrator
+ */
+@RestController
+@RequestMapping("/main")
+public class MainController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "")
+    public ModelAndView index() {
+        return new ModelAndView("main");
+        // return "main";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{name}")
+    public String getName(
+            @PathVariable String name
+    ) {
+        return "Hello " + name;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/some")
+    public String getSomeParam(
+            @RequestParam(value = "name", required = false, defaultValue = "World") String name
+    ) {
+        return "Some " + name;
+    }
+    @RequestMapping(method = RequestMethod.POST, value = "/some")
+    public String getSomeParamPost(
+            @RequestBody String name
+    ) {
+        return "Some " + name;
+    }
+}
